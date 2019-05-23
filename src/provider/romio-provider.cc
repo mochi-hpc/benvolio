e6929208b7a8b25c2f3b8c2a724d51fbab1ce8d5
Rob Latham <robl@mcs.anl.gov>
2019-05-23 16:28:18 -0500

    handle larger client requests
    Implement looping to repeatedly carry out I/O if client requests more
    data to read/write than the temporary buffer can hold.

--- src/provider/romio-provider.cc
+++ src/provider/romio-provider.cc
@@ -16,6 +16,8 @@ namespace tl = thallium;
 
 #define BUFSIZE 1024
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 struct romio_svc_provider : public tl::provider<romio_svc_provider>
 {
     tl::engine * engine;
@@ -44,16 +46,29 @@ struct romio_svc_provider : public tl::provider<romio_svc_provider>
     /* write:
      * - bulk-get into a contig buffer
      * - write out to file */
-    ssize_t process_write(const tl::request& req, tl::bulk &b, const std::string &file,
+    ssize_t process_write(const tl::request& req, tl::bulk &client_bulk, const std::string &file,
             std::vector<off_t> &file_starts, std::vector<uint64_t> &file_sizes)
     {
+        /* What else can we do with an empty memory description or file
+         description other than return immediately? */
+        if (client_bulk.size() == 0 ||
+                file_starts.size() == 0) {
+            req.respond(0);
+            return 0;
+        }
+
         /* cannot open read-only:
          - might want to data-sieve the I/O requests
          - might later read file */
         int flags = O_CREAT|O_RDWR;
         int fd = getfd(file, flags);
 
-        // expose bulk region
+        // we have a scratch buffer we can work with, which might be smaller
+        // than whatever the client has sent our way.  We will repeatedly bulk
+        // transfer into this region. We'll need to keep track of how many file
+        // offset/length pairs we have processed and how far into them we are.
+        // Code is going to start looking a lot like ROMIO...
+        //
         // TODO: configurable how many segments at a time we can process
         // ?? is there a way to get all of them?
         std::vector<std::pair<void *, std::size_t>>segments(1);
@@ -63,45 +78,113 @@ struct romio_svc_provider : public tl::provider<romio_svc_provider>
         tl::endpoint ep = req.get_endpoint();
         tl::bulk local = engine->expose(segments, tl::bulk_mode::read_write);
 
-        b.on(ep) >> local;
-        ssize_t xfered=0;
-        for (int i = 0; i< file_starts.size(); i++)
-            xfered += abt_io_pwrite(abt_id, fd, buffer+xfered, file_sizes[i], file_starts[i]);
+        // when are we done?
+        // - what if the client has a really long file descripton but for some reason only a small amount of memory?
+        // - what if the client has a really large amount of memory but a short file description?
+        // -- write returns the smaller of the two
+        size_t client_xfer=0, client_cursor=0, fileblk_cursor=0, buf_cursor=0;
+        size_t xfered=0, file_xfer=0, nbytes;
+        unsigned int file_idx=0;
+        // ceiling division: we'll do as many rounds of I/O as necessary given
+        // the intermediate buffer.  Note the one exception: if we run out of
+        // file description, we'll bail out early
+        size_t ntimes = 1 + (client_bulk.size() -1)/BUFSIZE;
 
+        for (unsigned int i = 0; i< ntimes; i++) {
+            // the '>>' operator moves bytes from one bulk descriptor to the
+            // other, moving the smaller of the two
+            file_xfer = 0;
+            client_xfer = client_bulk(client_cursor, client_bulk.size()-client_cursor).on(ep) >> local;
+            // operator overloading might make this a little hard to parse at first.
+            // - >> and << do a bulk transfer between bulk endpoints, transfering
+            //   the smallest  of the two
+            // - the '()' operator will select offset and length for a bulk region
+            //   if one wants a subset
+            // - select a subset on the client-side bulk descriptor before
+            //   associating it with a connection.
+            while (file_idx < file_starts.size() && file_xfer < client_xfer) {
+                // we might be able to only write a partial block
+                nbytes = MIN(file_sizes[file_idx]-fileblk_cursor, client_xfer-buf_cursor);
+                file_xfer += abt_io_pwrite(abt_id, fd, buffer+buf_cursor, nbytes, file_starts[file_idx]+fileblk_cursor);
+
+                if (nbytes + fileblk_cursor >= file_sizes[file_idx]) {
+                    file_idx++;
+                    fileblk_cursor = 0;
+                }
+                else
+                    fileblk_cursor += nbytes;
+
+                if (buf_cursor+nbytes < client_xfer)
+                    buf_cursor+=nbytes;
+                else
+                    buf_cursor=0;
+
+                xfered += nbytes;
+            }
+            client_cursor += client_xfer;
+        }
         req.respond(xfered);
         return 0;
     }
 
     /* read:
      * - read into contig buffer
-     * - bulk-put to client */
-    ssize_t process_read(const tl::request &req, tl::bulk &b, const std::string &file,
+     * - bulk-put to client
+     * as with write, might require multiple bulk-puts to complete if read
+     * request larger than buffer */
+    ssize_t process_read(const tl::request &req, tl::bulk &client_bulk, const std::string &file,
             std::vector<off_t> &file_starts, std::vector<uint64_t> &file_sizes)
     {
+        if (client_bulk.size() == 0 ||
+                file_starts.size() == 0) {
+            req.respond(0);
+            return 0;
+        }
         /* like with write, open for both read and write in case file opened
          * first for read then written to */
         int flags = O_RDWR;
 
         int fd = getfd(file, flags);
-        ssize_t xfered = 0;
-        for (int i= 0; i< file_starts.size(); i++) {
-            int ret = abt_io_pread(abt_id, fd, buffer+xfered, file_sizes[i], file_starts[i]);
-            if (ret == -1) {
-                perror("pread");
-                break;
-            }
-            xfered += ret;
-        }
-
-        std::vector<std::pair<void *, std::size_t>>segments(1);
-        segments[0].first = (void*)(&(buffer[0]));
-        segments[0].second = xfered;
 
         tl::endpoint ep = req.get_endpoint();
-        tl::bulk local = engine->expose(segments, tl::bulk_mode::read_only);
-        b.on(ep) << local;
+        /* Simliar algorithm as write, but data movement goes in the opposite direction */
+        unsigned file_idx=0;
+        size_t xfered = 0, file_xfer=0, client_xfer=0, nbytes;
+        size_t fileblk_cursor=0, buf_cursor=0, client_cursor=0;
+        // ceiling division.  Will bail out early if we exhaust file description
+        size_t ntimes = 1 + (client_bulk.size() - 1)/BUFSIZE;
+
+        for (unsigned int i = 0; i< ntimes; i++) {
+            file_xfer = 0;
+            while (file_idx < file_starts.size() && file_xfer < BUFSIZE) {
+                nbytes = MIN(file_sizes[file_idx]-fileblk_cursor, BUFSIZE-buf_cursor);
+                file_xfer += abt_io_pread(abt_id, fd, buffer+buf_cursor, nbytes, file_starts[file_idx]+fileblk_cursor);
+
+                if (nbytes + fileblk_cursor >= file_sizes[file_idx]) {
+                    file_idx++;
+                    fileblk_cursor=0;
+                }
+                else
+                    fileblk_cursor +=nbytes;
+
+                if (buf_cursor + nbytes < BUFSIZE)
+                    buf_cursor += nbytes;
+                else
+                    buf_cursor=0;
+
+                xfered += nbytes;
+            }
+            std::vector<std::pair<void *, std::size_t>>segments(1);
+            segments[0].first = (void*)(&(buffer[0]));
+            segments[0].second = file_xfer;
+            tl::bulk local = engine->expose(segments, tl::bulk_mode::read_write);
+
+            client_xfer = client_bulk(client_cursor, client_bulk.size()-client_cursor).on(ep) << local;
+            client_cursor += client_xfer;
+        }
         req.respond(xfered);
         return 0;
+
     }
 
     ssize_t stat(const std::string &file)
