6296ca9e8b6eaacf88d8effd880af3ed042fee74
Rob Latham <robl@mcs.anl.gov>
2019-07-12 16:29:07 +0000

    make lustre interrogation more robust
    work better for files that don't exist or are not lustre files

--- src/provider/lustre-utils.c
+++ src/provider/lustre-utils.c
@@ -9,7 +9,9 @@
 #endif
 
 #ifdef HAVE_LIBLUSTREAPI
-static void *alloc_lum()
+/* there are two(!!) kinds of data the lustre library might return, so we need
+ * something big enough to hold either one */
+static size_t get_lumsize()
 {
     int v1, v3;
 
@@ -18,7 +20,7 @@ static void *alloc_lum()
     v3 = sizeof(struct lov_user_md_v3) +
         LOV_MAX_STRIPE_COUNT * sizeof(struct lov_user_ost_data_v1);
 
-    return malloc(MAX(v1, v3));
+    return MAX(v1, v3);
 }
 #endif
 
@@ -27,20 +29,42 @@ int lustre_getstripe(const char * filename, int32_t *stripe_size, int32_t *strip
 {
     int ret = 0;
     /* guess some reasonable defaults for non-lustre systems */
-    *stripe_size = 4092;
+    *stripe_size = 4096;
     *stripe_count = 1;
 
 #ifdef HAVE_LIBLUSTREAPI
     struct lov_user_md *lov;
-    lov = alloc_lum();
+    lov = alloca(get_lumsize());
 
+    /* - maybe the file wasn't there?
+     *	  - check parent directory
+     * - maybe the file is not lustre? */
+    ret = llapi_file_get_stripe(filename, lov);
+    switch(errno) {
+	case ENOENT:
+	    char * dup = strdup(filename);
+	    char * parent = dirname(dup);
+	    ret = llapi_file_get_stripe(filename, lov);
+	    /* if still enoent, we give up */
+	    if (errno ==  ENOENT)
+		goto fn_exit;
+	case ENOTTY:
+	    ret = 0;
+	    goto fn_exit;
+	default:
+	    perror("Unable to get Lustre stripe info");
+    }
     if (llapi_file_get_stripe(filename, lov) != 0) {
 	perror("Unable to get Lustre stripe info");
 	return -1;
     };
     *stripe_size = lov->lmm_stripe_size;
     *stripe_count = lov->lmm_stripe_count;
+
+    /* hack for demo */
+    *stripe_size = 4096;
 #endif
 
+fn_exit:
     return ret;
 }
