0f0928fb1fabaa61dc50783af972432a76f88adc
qiao.kang@eecs.northwestern.edu <qkt561@guru4.ece.northwestern.edu>
2020-08-19 01:31:51 -0500

    test

--- src/provider/bv-provider.cc
+++ src/provider/bv-provider.cc
@@ -650,7 +650,7 @@ static void cache_partition_request(Cache_file_info *cache_file_info, const std:
         cache_offset = *it;
         //printf("cache offset = %llu\n", (long long unsigned)cache_offset);
         cache_size2 = MIN(cache_size, stripe_size - (cache_offset % stripe_size));
-#if 1==0
+
         for ( i = 0; i < file_starts.size(); ++i ) {
             if (file_starts[i] >= cache_offset && file_starts[i] < cache_offset + cache_size2) {
                 //Start position inside cache page.
@@ -681,7 +681,7 @@ static void cache_partition_request(Cache_file_info *cache_file_info, const std:
                 //break;
             }
         }
-#endif
+
     }
 
     delete pages;
