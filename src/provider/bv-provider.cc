d8e73ca8164c3e55fc1d310de729671df17c3476
qiao.kang@eecs.northwestern.edu <qkt561@guru4.ece.northwestern.edu>
2020-08-21 09:46:37 -0500

    test

--- src/provider/bv-provider.cc
+++ src/provider/bv-provider.cc
@@ -771,7 +771,7 @@ static void cache_register(Cache_info *cache_info, std::string file, Cache_file_
         /* When we are out of cache space, we are going to remove all file caches that are not currently processed.*/
         if (cache_info->cache_block_used[0] >= BENVOLIO_CACHE_MAX_N_BLOCKS) {
             printf("ssg_rank %d entered eager cache write-back mechanism! page used = %ld, budget = %ld\n", cache_info->ssg_rank, cache_info->cache_block_used[0], BENVOLIO_CACHE_MAX_N_BLOCKS);
-            cache_flush_all(cache_info, 0);
+            //cache_flush_all(cache_info, 0);
         }
 
         cache_file_info->cache_block_reserved = MAX(BENVOLIO_CACHE_MIN_N_BLOCKS, (BENVOLIO_CACHE_MAX_N_BLOCKS - cache_info->cache_block_used[0])/2);
@@ -1113,24 +1113,28 @@ static void cache_flush_array(Cache_file_info *cache_file_info, const std::vecto
 */
         //Remove memory and table entry
         if ( cache_file_info->cache_table->find(cache_offset) != cache_file_info->cache_table->end() ) {
+/*
             free(cache_file_info->cache_table[0][cache_offset]->second);
             delete cache_file_info->cache_table[0][cache_offset];
             cache_file_info->cache_table->erase(cache_offset);
+*/
         } else {
             printf("cache table is empty !!!!!!!!!!!\n");
         }
         
         if ( cache_file_info->cache_page_mutex_table->find(cache_offset) != cache_file_info->cache_page_mutex_table->end() ) {
+/*
             delete cache_file_info->cache_page_mutex_table[0][cache_offset]->second;
             delete cache_file_info->cache_page_mutex_table[0][cache_offset];
             cache_file_info->cache_page_mutex_table->erase(cache_offset);
+*/
         } else {
             printf("cache mutex table is empty !!!!!!!!!!!\n");
         }
 
         std::vector<off_t>::iterator it3 = std::find(cache_file_info->cache_offset_list->begin(), cache_file_info->cache_offset_list->end(), cache_offset);
         if (it3!=cache_file_info->cache_offset_list->end()) {
-             //cache_file_info->cache_offset_list->erase(it3);
+             cache_file_info->cache_offset_list->erase(it3);
         } else {
             printf("cache offset list is empty !!!!!!!!!!!\n");
         }
@@ -2893,7 +2897,7 @@ struct bv_svc_provider : public tl::provider<bv_svc_provider>
             }
             test_sum = 0;
             test_max = 0;
-            printf("implementation version v2, ssg_rank %d initialized with BENVOLIO_CACHE_MAX_N_BLOCKS = %d, BENVOLIO_CACHE_MIN_N_BLOCKS = %d, BENVOLIO_CACHE_MAX_BLOCK_SIZE = %d\n", ssg_rank, BENVOLIO_CACHE_MIN_N_BLOCKS, BENVOLIO_CACHE_MAX_N_BLOCKS, BENVOLIO_CACHE_MAX_BLOCK_SIZE);
+            printf("implementation version v3, ssg_rank %d initialized with BENVOLIO_CACHE_MAX_N_BLOCKS = %d, BENVOLIO_CACHE_MIN_N_BLOCKS = %d, BENVOLIO_CACHE_MAX_BLOCK_SIZE = %d\n", ssg_rank, BENVOLIO_CACHE_MIN_N_BLOCKS, BENVOLIO_CACHE_MAX_N_BLOCKS, BENVOLIO_CACHE_MAX_BLOCK_SIZE);
 
             ABT_thread_create(pool.native_handle(), cache_resource_manager, &rm_args, ABT_THREAD_ATTR_NULL, NULL);
             ABT_eventual_create(0, &rm_args.eventual);
