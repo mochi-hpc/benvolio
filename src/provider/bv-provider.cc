f91fdb918c6ffc1534c4d5740df899c04345d36c
qiao.kang@eecs.northwestern.edu <qkt561@guru4.ece.northwestern.edu>
2020-08-21 19:14:31 -0500

    test

--- src/provider/bv-provider.cc
+++ src/provider/bv-provider.cc
@@ -1278,8 +1278,8 @@ static void cache_allocate_memory(Cache_file_info *cache_file_info, off_t file_s
             cache_size2 = MIN(cache_size, stripe_size - i * cache_size);
 
             // This region is the maximum possible cache, we may not necessarily use all of it, but we can adjust size later without realloc.
-            cache_file_info->cache_table[0][cache_offset]->second = (char*) malloc(sizeof(char) * cache_size2);
-            //cache_file_info->cache_table[0][cache_offset]->second = (char*) malloc(sizeof(char) * 65536);
+            //cache_file_info->cache_table[0][cache_offset]->second = (char*) malloc(sizeof(char) * cache_size2);
+            cache_file_info->cache_table[0][cache_offset]->second = (char*) malloc(sizeof(char) * 65536);
             if (test_max < cache_size2) {
                 test_max = cache_size2;
             }
